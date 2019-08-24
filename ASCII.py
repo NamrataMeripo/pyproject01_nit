473ec4c8eaa2b59acd10e8bde6a5ad365892ce92
NamrataMeripo <namrata.meripo@gmail.com>
2019-08-24 19:48:50 -0400

    updatedinfo

---
+++ ASCII.py
@@ -0,0 +1,8 @@
+ASCII - American standard code for information interchange
+
+1) Dec = decimal value (0 to 27)
+2) Char = character (null to delete button)
+3) numeric = 48 to 57
+4) uppercase = 65 to 90
+5) lowercase = 97 to 122
+
