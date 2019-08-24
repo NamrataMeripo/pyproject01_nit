c2e34ee9037760796586cc4091c595766c591270
NamrataMeripo <namrata.meripo@gmail.com>
2019-08-24 19:39:54 -0400

    upload day5

---
+++ 20190823-Day5.py
@@ -0,0 +1,33 @@
+
+
+#!/usr/bin/python
+
+# Creating variables for python
+
+firstname = 'Guido'
+print(firstname)
+
+MIDDLENAME = "Van"
+print(MIDDLENAME)
+
+lastNAME = "Rossum"
+print(lastNAME)
+
+a0001 = 'Joel'
+print(a0001)
+
+_0001 = 'Apple'
+print(_0001)
+
+raw_data = ''' we can use single tripples for paragraphs '''
+print(raw_data)
+
+#Usage of double tripples
+
+Processed_data = """ We can 
+use double tripples 
+as well """
+print(Processed_data)
+
+""" This is a multiple line 
+comments """
