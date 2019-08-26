36b3bc900beadc88afdd7afec284c9c35c3b8721
NamrataMeripo <namrata.meripo@gmail.com>
2019-08-26 17:03:24 -0400

    updatedinfo

---
+++ 20190826-Day6.py
@@ -0,0 +1,18 @@
+
+
+"""
+  325  cd pyproject01_nit
+  326  git fetch
+  327  git pull
+  328  git branch -r
+  329  git checkout NamrataMeripo
+  330  git branch
+  331  git checkout master
+  332  git status
+  333  git branch
+  334  git fetch
+  335  git pull
+  336  git history
+  337  history
+
+"""
