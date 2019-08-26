b64e69ae04bef94b69333c992444ebdd33cc2b28
NamrataMeripo <namrata.meripo@gmail.com>
2019-08-26 18:34:33 -0400

    updateddataypes

---
+++ datatypes.py
@@ -0,0 +1,18 @@
+
+
+#!/usr/bin/python
+
+# Numbers
+
+# int - 0 to 9
+account_id = 19089
+print(account_id, type(account_id))
+
+account_id = 9899.098
+print(account_id, type(account_id))
+
+unexpectedprice = 4+5j
+print(unexpectedprice, type(unexpectedprice))
+
+
+
