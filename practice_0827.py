d5c03e596bd14c8c49aea339fac4044396c8a411
NamrataMeripo <namrata.meripo@gmail.com>
2019-08-27 16:10:57 -0400

    practicesession

---
+++ practice_0827.py
@@ -0,0 +1,23 @@
+
+#!/usr/bin/python
+
+
+accountname = "Namrata Meripo"
+bankname = "BankofAmerica"
+print(accountname + bankname)
+print(accountname +  "  bankname")
+
+print(accountname *3)
+print(len(accountname *3))
+
+
+print(accountname[8])
+print(accountname[-5] ,id(accountname))
+
+
+
+
+
+
+
+
