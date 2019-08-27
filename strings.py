a7dc7723c79000dac223494c60fa5d3dac008ce1
NamrataMeripo <namrata.meripo@gmail.com>
2019-08-26 22:43:27 -0400

    uploadnewinfo

---
+++ strings.py
@@ -0,0 +1,19 @@
+
+
+
+# creating the strings
+
+username = "Namrata"
+#print(username,type(username),id(username))
+
+#print((username),type(username),id(username)))
+
+
+print(username)
+
+print(type(username))
+
+print(id(username))
+
+print(username,type(username),id(username))
+
---
+++ stringspecialoperators.py
@@ -0,0 +1,36 @@
+
+
+
+#!/usr/bin/python
+
+# + - concatination
+
+bankname= "bankofamerica"
+branchdetails= "Atlanta"
+print(bankname + branchdetails)
+print(bankname, branchdetails)
+print(bankname + "   localbranch")
+print("branchname")
+
+
+#  * - Repeatetion
+
+accountholder = "Namrata Meripo"
+print(len(accountholder))
+print(accountholder * 3)
+
+
+
+# [] - Slicing
+
+accounholder = "Namrata Meripo"
+
+print(accounholder [8])
+
+
+
+
+# [] Slicing 
+
+
+
