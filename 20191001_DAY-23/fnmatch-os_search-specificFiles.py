db594270106f432c0c61156deab2b45e47804c5b
NamrataMeripo <54295343+NamrataMeripo@users.noreply.github.com>
2019-11-15 16:14:40 -0500

    Update

--- 20191001_DAY-23/fnmatch-os_search-specificFiles.py
+++ 20191001_DAY-23/fnmatch-os_search-specificFiles.py
@@ -6,7 +6,7 @@ images = ['*.jpg', '*.jpeg', '*.png', '*.tif', '*.tiff']
 
 matches = []
 
-for dirpath,dirnames,filenames in os.walk('/Users/keshavkummari/pyproject01_nit/20191001_DAY-23'):
+for dirpath,dirnames,filenames in os.walk('/c/Python/pyproject01_nit/newpractice'):
     for extentions in images:
         for i in fnmatch.filter(filenames,extentions):
             matches.append(os.path.join(dirpath,i))
