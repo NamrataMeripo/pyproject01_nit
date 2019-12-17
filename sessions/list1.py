aa11bb7c9ae0b571d0b25984fde2d36554604990
NamrataMeripo <54295343+NamrataMeripo@users.noreply.github.com>
2019-12-16 20:40:28 -0500

    update

---
+++ sessions/list1.py
@@ -0,0 +1,10 @@
+a = 10 
+b = 1
+sum = 0
+
+while a >b :
+    for varchar in "AMERICA" :
+        print("Countryname",varchar)
+    b = b+1
+    print(b)
+
---
+++ sessions/session1.py
@@ -0,0 +1,15 @@
+#usr/bin/Python
+
+balance = 10
+
+def namebank () :
+    bankname = "BOA"
+    print(bankname)
+    print("I am on session1.py")
+    
+
+if __name__ == "__main__":
+    print("Executing main program")
+
+
+
